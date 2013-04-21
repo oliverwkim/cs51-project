845d66aea88bce9b9aedefa70be9954f19d06d79
Roger Huang <rogerghuang@gmail.com>
2013-04-20 20:51:31 -0400

    deleted

--- CS51Project/src/gridInterface.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Point;
-public interface GridInterface {
-	
-	// Used to get specific Nodes from inside a grid
-	Node getNode(Point c);
-
-	void createStandard(int size);
-	void createRandom(int size);
-}
