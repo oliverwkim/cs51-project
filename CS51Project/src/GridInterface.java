8f0a4ed0e6842ff9e7c3662b748872292769b982
Jonathan Ward <jward@college.harvard.edu>
2013-04-20 21:02:58 -0400

    a message

--- CS51Project/src/GridInterface.java
+++ CS51Project/src/GridInterface.java
@@ -2,7 +2,7 @@ import java.awt.Point;
 public interface GridInterface{
 	// Used to get specific Nodes from inside a grid
 	Node getNode(Point c);
-	
+	Bool hasNode(Point c);
 	void createStandard(int size);
 	void createRandom(int size);
 }
