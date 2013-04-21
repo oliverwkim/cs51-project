b55ac71604e232472b1957b85a0a8964d516d844
Roger Huang <rogerghuang@gmail.com>
2013-04-20 20:53:53 -0400

    Added proper caps

---
+++ CS51Project/src/GridInterface.java
@@ -0,0 +1,8 @@
+import java.awt.Point;
+public interface GridInterface{
+	// Used to get specific Nodes from inside a grid
+	Node getNode(Point c);
+	
+	void createStandard(int size);
+	void createRandom(int size);
+}
