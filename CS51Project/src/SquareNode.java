7b671013977532afd0d960acddff0404a3d03712
Roger Huang <rogerghuang@gmail.com>
2013-04-20 20:35:02 -0400

    Testing

--- CS51Project/src/SquareNode.java
+++ CS51Project/src/SquareNode.java
@@ -2,20 +2,22 @@ public class SquareNode extends Node {
 
 	// Maximum number of nodes this can be connected to
 	private static final int maxConnections = 4;
-	private int marker = 0;
+	private int marker;
 
 	
-	// hi Roger
 	public SquareNode(){
 		super(maxConnections);
+		marker = 0;
 	}
 
 	public SquareNode(int x, int y, boolean pass){
 		super(x, y, pass, maxConnections);
+		marker = 0;
 	}
 
 	public SquareNode(int x, int y, int cost, boolean pass){
 		super(x, y, cost, pass, maxConnections);
+		marker = 0;
 	}
 
 	void linkNode(Node n) {
