794539202934076a39f019422195ea976c3b6a8d
Jonathan Ward <jward@college.harvard.edu>
2013-04-26 14:47:08 -0400

    reconstruct path adjusted

--- CS51Project/src/LPAstar.java
+++ CS51Project/src/LPAstar.java
@@ -2,24 +2,23 @@ import java.util.PriorityQueue;
 import java.util.Comparator;
 import java.util.ArrayList;
 
-public class LPAStar extends AStar {
+public class LPAStar extends Astar { // for the hScore method
 
 	public static int[] calculateKey(Node s, Node goal) 
 	{
-		return [min(s.getG(), s.getRhs() + hscoreH(s,goal)); min(s.getG(), s.getRhs())];
+		return [min(s.getG(), s.getRhs() + hScore(s,goal)); min(s.getG(), s.getRhs())];
 	}
 
-	public static PriorityQueue<Node> initialize(Grid g, Node start)
+	public static void initialize(Grid g, Node start)
 	{
 		PriorityQueue<Pair> open_set = new PriorityQueue<Pair>(11, comparator); 
-		for (Node s : start.getConnections()) 
+		for (Node s : start.getAllVisible()) 
 		{
 			s.setGScore(-1); 
 			s.setRhsScore(-1);
 		}
 		start.setRhsScore(0);
 		open_set.add(s,[s.hscore();0]);
-		return open_set;
 	}
 
 	public static void updateVertex(Node u, Grid g, PriorityQueue open_set, Mode start)
@@ -41,10 +40,10 @@ public class LPAStar extends AStar {
 		while((open_set.peek() < calculateKey()) || (goal.getRhs() <> goal.getG()))
 		{
 			Node u = open_set.pop();
-			if (u.getG() > u.getRhs()) 
+			if (u.getG() > u.getRhs())
 			{
 				u.setGScore(u.getRhs());
-				for (Node s : u.getConnections()) 
+				for (Node s : u.getConnections())
 					updateVertex(s, g, open_set);
 			}
 			else
@@ -52,20 +51,15 @@ public class LPAStar extends AStar {
 				u.setGScore(-1);
 				for (Node s : g.getAdjacent(u)) 
 					updateVertex(s, g, open_set);
-				updateVertex(u);
+				updateVertex(u, g, open_set);
 			}
 		}	
 	}
 
 	public static void algorithm(Grid g, Node goal, Node start)
 	{
-		open_set = initialize(g, start);
-		forever
-			computeShortestPath(open_set, goal, g)
-			//Wait for changes in edge costs
-			//for all edges (u,v) with changed edge costs
-				//update edge cost c(u,v)
-				//updateVertex(v)
+		initialize(g, start);
+		computeShortestPath(open_set, goal, g);
 	}
 
 	public static Node[] reconstructPath(Node goal, Node start, Grid g)
@@ -81,8 +75,10 @@ public class LPAStar extends AStar {
 		{
 			PriorityQueue<int> values = new PriorityQueue<int>;
 			for (Node s : goal.getConnections))
-				values.add(s.getG() + g.edgelength)
-
+				values.add(s.getG() + g.edgelength);
+			minval =values.peek();
+			path.add(minval);
+			reconstructPath(goal, minval, g);
 		}
 	}
 
