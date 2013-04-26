92a6750362bd4150a09193a11b0b2b64ea8b2c4b
Jonathan Ward <jward@college.harvard.edu>
2013-04-25 20:25:29 -0400

    LPAstar things

---
+++ CS51Project/src/LPANodeComparator.java
@@ -0,0 +1,3 @@
+public class LPAPairComparator extends NodeComparator{
+	public int compare()
+}
---
+++ CS51Project/src/LPAPairComparator.java
@@ -0,0 +1,5 @@
+public class LPAPairComparator extends NodeComparator{
+	public int compare(Pair pair1, Pair pair2){
+		i
+	}
+}
