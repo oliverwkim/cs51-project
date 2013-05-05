3a9463dd80569a6755b434949fb34ff59e1a71d0
oliverwkim <oliverwkim@gmail.com>
2013-05-04 22:06:40 -0400

    major gui changes that are really awesome

--- CS51Project/src/ControlPanel.java
+++ CS51Project/src/ControlPanel.java
@@ -4,10 +4,21 @@ import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 public class ControlPanel extends JPanel {
+	
+	private static JTextField heightField;
+	private static JTextField widthField;
+
+	private static JTextField startXField;
+	private static JTextField startYField;
+	private static JTextField endXField;
+	private static JTextField endYField;
+	
 	public static void main(String args[])
 	{
 		JFrame f = new JFrame();
@@ -15,14 +26,51 @@ public class ControlPanel extends JPanel {
 		p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
 		
 		JButton generateButton = new JButton("Generate Random Grid");
+		JLabel heightLabel = new JLabel("Grid height");
+		heightField = new JTextField("10");
+		JLabel widthLabel = new JLabel("Grid width");
+		widthField = new JTextField("10");
+		
+		JLabel startXLabel = new JLabel("Starting X");
+		startXField = new JTextField("0");
+		JLabel startYLabel = new JLabel("Starting Y");
+		startYField = new JTextField("0");
+		
+		JLabel endXLabel = new JLabel("Ending X");
+		endXField = new JTextField("5");
+		JLabel endYLabel = new JLabel("Ending Y");
+		endYField = new JTextField("5");
+		
 		generateButton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e)
 			{
-					new GUIPanel(20,30,40);
-
+				int height = Integer.parseInt(heightField.getText());
+				int width = Integer.parseInt(widthField.getText());
+				int startX = Integer.parseInt(startXField.getText());
+				int startY = Integer.parseInt(startYField.getText());
+				int endX = Integer.parseInt(endXField.getText());
+				int endY = Integer.parseInt(endYField.getText());
+				new GUIPanel(20,30,40, height, width, startX, startY, endX, endY);
 			}
 		});
+		
 		p.add(generateButton);
+		p.add(heightLabel);
+		p.add(heightField);
+		p.add(widthLabel);
+		p.add(widthField);
+		
+		p.add(startXLabel);
+		p.add(startXField);
+		p.add(startYLabel);
+		p.add(startYField);
+		
+		p.add(endXLabel);
+		p.add(endXField);
+		p.add(endYLabel);
+		p.add(endYField);
+		
+		f.setTitle("Pathfinding Control Panel");
 		f.setContentPane(p);
 		f.pack();
 		f.setVisible(true);
